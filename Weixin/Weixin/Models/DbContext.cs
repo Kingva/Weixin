cd0f83ceff79ff3623c8ec0a312ee59529356f14
qingfawang <qingfawang@outlook.com>
2016-03-30 18:08:20 +0800

    init spike

---
+++ Weixin/Weixin/Models/DbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weixin.Models
+{
+    public class AdAuditingContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public AdAuditingContext() : base("name=AdAuditingContexta")
+        {
+        }
+
+//        public DbSet<Material> Materials { get; set; }
+//        public DbSet<AdClassification> AdClassifications { get; set; }
+//        public DbSet<AuditComments> AuditComments { get; set; }
+//        public DbSet<AuditRound> AuditRound { get; set; }
+//        public DbSet<AuditingResult> AuditingResult { get; set; }
+//        public DbSet<LegalProvision> LegalProvision { get; set; }
+//        public DbSet<MediaOwnerGroup> MediaOwnerGroup { get; set; }
+
+    }
+}
---
+++ Weixin/Weixin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Weixin.Startup))]
+namespace Weixin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
